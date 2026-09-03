90ac132f0fa608a19c8b92f4f8b18b5582475647
Stealthness <stevewestinlondon@gmail.com>
2026-09-03 04:33:57 +0100

    created target Spawner

--- Simple Mouse Clicker/Assets/Scripts/TargetSpawner.cs
+++ Simple Mouse Clicker/Assets/Scripts/TargetSpawner.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TargetSpawner : MonoBehaviour
 {
     public static TargetSpawner Instance { get; private set; }
-    
-    private HashSet<GameObject> _activeTargets = new HashSet<GameObject>();
+    public GameObject targetPrefab;
+
+    private HashSet<GameObject> _targets;
     
 
     private void Awake()
     {
         Instance = this;
+        _targets = new HashSet<GameObject>();
+        for (int i = 0; i < 5; i++)
+        {
+            _targets.Add(CreateTarget());
+        }
     }
     
     public void StartSpawning()
@@ -21,6 +28,22 @@ public class TargetSpawner : MonoBehaviour
 
     private void SpawnTarget()
     {
-        Debug.Log("Target spawned.");
+        foreach (var target in _targets.Where(target => !target.activeInHierarchy))
+        {
+            target.SetActive(true);
+            return;
+        }
+
+        _targets.Add(CreateTarget());
     }
+    
+    private GameObject CreateTarget()
+    {
+        var randomPosition = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
+        var target = Instantiate(targetPrefab, randomPosition, Quaternion.identity, transform);
+        target.SetActive(false);
+        return target;
+    }
+
+ 
 }
