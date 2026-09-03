0d310661032a74a7c34c1ea113a444c10c0c9718
Stealthness <stevewestinlondon@gmail.com>
2026-09-03 04:12:40 +0100

    added Menu

--- Simple Mouse Clicker/Assets/Scripts/GameManager.cs
+++ Simple Mouse Clicker/Assets/Scripts/GameManager.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        Debug.Log("GameManager started.");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
