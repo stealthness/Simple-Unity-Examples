c1f9fe5c9e27766438de7dc7b27d5c745cc53e4f
Stealthness <stevewestinlondon@gmail.com>
2026-09-03 04:19:11 +0100

    create start game function

--- Simple Mouse Clicker/Assets/Scripts/GameManager.cs
+++ Simple Mouse Clicker/Assets/Scripts/GameManager.cs
@@ -1,10 +1,26 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject MenuPanel;
+
+    private void Awake()
+    {
+        MenuPanel.SetActive(true);
+    }
+
+
     private void Start()
     {
         Debug.Log("GameManager started.");
     }
-
+    
+    
+    public void OnStartButtonClick()
+    {
+        Debug.Log("Start button clicked.");
+        MenuPanel.SetActive(false);
+        TargetSpawner.Instance.StartSpawning();
+    }
 }
---
+++ Simple Mouse Clicker/Assets/Scripts/TargetSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour
+{
+    public static TargetSpawner Instance { get; private set; }
+    
+    private HashSet<GameObject> _activeTargets = new HashSet<GameObject>();
+    
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    public void StartSpawning()
+    {
+        Debug.Log("TargetSpawner started spawning targets.");
+        InvokeRepeating(nameof(SpawnTarget), 0f, 2f);
+    }
+
+    private void SpawnTarget()
+    {
+        Debug.Log("Target spawned.");
+    }
+}
