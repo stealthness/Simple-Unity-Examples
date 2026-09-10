31eddf7c7d096457803acff6e1ac85ce1ecfa469
Stealthness <stevewestinlondon@gmail.com>
2026-09-10 14:57:55 +0100

    Repurpose form existing scripts

---
+++ Simple Tileset Example/Assets/Main/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Scripts.Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(PlayerInput))]
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(PlayerHealth))]
+    [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class PlayerController : MonoBehaviour
+    {
+        private SpriteRenderer _sr;
+        private AudioSource _audioSource;
+        private Rigidbody2D _rigidbody2D;
+        private Vector2 _moveDirection;
+        private PlayerHealth _playerHealth;
+        [SerializeField] private AudioClip footAudioClip;
+        [SerializeField] private AudioClip hurtAudioClip;
+        [SerializeField] private float playerSpeed = 3f;
+
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+            _rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+            _sr = GetComponent<SpriteRenderer>();
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        private void Start()
+        {
+        }
+
+        public void OnMove(InputValue inputValue)
+        {
+
+            _moveDirection = inputValue.Get<Vector2>().normalized;
+            _rigidbody2D.linearVelocity = _moveDirection * playerSpeed;
+
+            
+        
+        }
+        
+
+
+        private void FixedUpdate()
+        {
+            CheckDirection(_moveDirection.x);
+            
+            if (_moveDirection.magnitude > 0.1f && !_audioSource.isPlaying )
+            {
+                
+                Debug.Log("Start : " + _moveDirection.magnitude);
+                // Play walking sound
+                _audioSource.PlayOneShot(footAudioClip);
+            }
+            if (_moveDirection.magnitude <= 0.1f && _audioSource.isPlaying && _audioSource.clip.name == footAudioClip.name)
+            {
+                
+                Debug.Log("Stop : " + _moveDirection.magnitude);
+                // Stop walking sound
+                _audioSource.Stop();
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+                // Handle collision with enemy
+                Debug.Log("Player collided with enemy!");
+                // You can add logic here to damage the player or trigger an event
+                PlayerHealth playerHealth = GetComponent<PlayerHealth>();
+                _audioSource.PlayOneShot(hurtAudioClip);
+                if (other.gameObject.name.Contains("Bear"))
+                {
+                    playerHealth.TakeDamage(30); // Example damage
+                }
+                else if (other.gameObject.name.Contains("Turtle"))
+                {
+                    playerHealth.TakeDamage(20);
+                }
+                else if (other.gameObject.name.Contains("Crab"))
+                {
+                    playerHealth.TakeDamage(5);
+                }
+                else
+                {
+                    playerHealth.TakeDamage(1);
+                }
+                
+            }
+        }
+        
+        /// <summary>
+        /// Checks the direction of the player and flips the sprite appropriately 
+        /// </summary>
+        /// <param name="dir">The dire of the player</param>
+        private void CheckDirection(float dir)
+        {
+            if (dir < 0)
+            {
+                _sr.flipX = true;
+            }
+
+            if (dir > 0)
+            {
+                _sr.flipX = false;
+            }
+        }
+
+        public void OnPlayerDeath()
+        {
+            _audioSource.Stop();
+        }
+
+
+    }
+    
+}
---
+++ Simple Tileset Example/Assets/Main/_Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,88 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace _Scripts.Player
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class PlayerHealth : MonoBehaviour
+    {
+        
+        [SerializeField] private Color playerColor;
+        
+        [SerializeField] private int maxHealth = 100;
+        [SerializeField] private float invulnerabilityTime = 0.5f;
+            
+        private int _currentHealth;
+        private SpriteRenderer _sr;
+        
+        private bool _playerIsInvulnerable = false;
+        
+        
+        public GameObject DeadPlayerPrefab;
+        public TextMeshProUGUI playerHealthText;
+
+        private void Awake()
+        {
+            _sr = GetComponent<SpriteRenderer>();
+        }
+
+        private void Start()
+        {
+            _currentHealth = maxHealth;
+            UpdateHealthUI();
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (_playerIsInvulnerable)
+                return;
+            
+            TakeHitDamage(damage);
+        }
+
+        private void TakeHitDamage(int damage)
+        {
+            _currentHealth -= damage;
+            if (_currentHealth <= 0)
+            {
+                _currentHealth = 0;
+                UpdateHealthUI();
+                Die();
+            }
+
+            UpdateHealthUI();
+            playerColor = _sr.color;
+            _sr.color = Color.red + playerColor;
+            Invoke(nameof(RemoveInvulnerability), invulnerabilityTime);
+        }
+
+
+        private void RemoveInvulnerability()
+        {
+            _playerIsInvulnerable = false;
+            _sr.color = playerColor;
+        }
+
+        private void UpdateHealthUI()
+        {
+            playerHealthText.text = $"Health: {_currentHealth}/{maxHealth}";
+        }
+
+        public void Die()
+        {
+            _sr.color = Color.black;
+            //UIManager.Instance.ShowGameLost("You lost all you health");
+            Instantiate(DeadPlayerPrefab, transform.position, Quaternion.identity);
+            //GameManager.Instance.GameOver();
+        }
+
+        public void SetColor(Color color)
+        {
+            playerColor = color;
+            if (_sr.color != Color.red)
+            {
+                _sr.color = color;
+            }
+        }
+    }
+}
